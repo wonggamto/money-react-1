be896dc88d64c79eba3efbced5242060cdd31d04
wonggamto <wonggamto@github.com>
2021-04-10 13:44:03 +0800

    封装 Button 组件

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  border: none;
+  font-size: 18px;
+  padding: 8px 12px;
+  border-radius: 4px;
+  background: #F60;
+  color: white;
+
+`;
+export {Button};
