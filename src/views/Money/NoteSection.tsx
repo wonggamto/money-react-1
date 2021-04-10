4e4506b01458471cc0122e3a83ec3dda361e979e
wonggamto <wonggamto@github.com>
2021-04-10 23:18:49 +0800

    创建 单独的Input 组件

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,29 +1,12 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0px 16px;
   font-size: 14px;
 
-  > label {
-    display: flex;
-    align-items: center;
-
-    > span {
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-
-    > input {
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-
-    }
-  }
 `;
 //函数组件 参数类型
 type Props = {
@@ -31,14 +14,13 @@ type Props = {
     onChange: (value: string) => void
 }
 const NoteSection: React.FunctionComponent<Props> = (props) => {
-   const note = props.value
+    const note = props.value;
+    const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+        props.onChange(e.target.value);
+    };
     return (
         <Wrapper>
-            <label>
-                <span>备注</span>
-                <input type="text" placeholder="在这里添加备注" value={note}
-                       onChange={(e) => {props.onChange(e.target.value);}}/>
-            </label>
+            <Input type="text" label="备注" value={note} onChange={onChange}/>
         </Wrapper>
     );
 };
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 
 import {Icon} from '../components/Icon';
 import {Link} from 'react-router-dom';
+import {Button} from '../components/Button';
 
 const TagList = styled.ol`
   font-size: 16px;
@@ -34,15 +35,7 @@ const TagList = styled.ol`
   }
 
 `;
-const Button = styled.button`
-  border: none;
-  font-size: 18px;
-  padding: 8px 12px;
-  border-radius: 4px;
-  background: #F60;
-  color: white;
 
-`;
 //居中标签
 const Center = styled.div`
   display: flex;
